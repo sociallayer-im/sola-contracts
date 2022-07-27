de3fec76a3c5ed9893bdc5c1e5139e1aced708e2
jiangplus <jiang.plus.times@gmail.com>
2022-07-28 04:47:30 +0800

    add upgradeable NFT contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import '@openzeppelin/hardhat-upgrades';
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
 };
