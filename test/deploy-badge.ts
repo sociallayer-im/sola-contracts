980a10b7ad09f84430d52886d1bb225a19df3e26
jiangplus <jiang.plus.times@gmail.com>
2022-12-15 05:07:45 +0800

    update badge contract

--- test/deploy-badge.ts
+++ test/deploy-badge.ts
@@ -4,29 +4,26 @@ import { ethers, upgrades } from "hardhat";
 describe("Badge", function () {
 
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
+    it("Should be able to mint", async function () {
       const [owner, otherAccount] = await ethers.getSigners();
 
-      const Org = await ethers.getContractFactory("Org");
-      let org = await upgrades.deployProxy(Org, []);
-      await org.deployed();
-      console.log("org deployed to:", org.address);
-
       const Badge = await ethers.getContractFactory("Badge");
-      let badge = await upgrades.deployProxy(Badge, [org.address]);
+      let badge = await upgrades.deployProxy(Badge, ["http://nft.solas.im/meta/"]);
       await badge.deployed();
       console.log("badge deployed to:", badge.address);
 
-      const Subject = await ethers.getContractFactory("Subject");
-      let subject = await upgrades.deployProxy(Subject, [org.address]);
-      await subject.deployed();
-      console.log("subject deployed to:", subject.address);
-
-      await badge.mint(owner.address, otherAccount.address, 0, 0, 0)
+      await badge.mint(otherAccount.address, owner.address)
       console.log('owner', await badge.ownerOf(1))
+      console.log('uri', await badge.tokenURI(1))
 
-      await badge.transferFrom(owner.address, otherAccount.address, 1)
-      console.log('owner', await badge.ownerOf(1))
+      await badge.mintBatch([otherAccount.address], [owner.address])
+      console.log('owner', await badge.ownerOf(2))
+
+      await badge.mintWithId([otherAccount.address], [owner.address], [100])
+      console.log('owner', await badge.ownerOf(100))
+
+      // await badge.transferFrom(otherAccount.address, owner.address, 1)
+      // console.log('owner', await badge.ownerOf(1))
     });
   });
 });
